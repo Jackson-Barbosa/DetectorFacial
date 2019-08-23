4d0c6f4a994d9d5964a42180ab7539a4028c45e6
Jackson S Barbosa <jackson_silva31@hotmail.com>
2019-08-22 23:59:47 -0300

    Primeira importacao.

---
+++ DetectorFacial/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DetectorFacial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.cameraBox = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Location = new System.Drawing.Point(144, 320);
+            this.btnIniciar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 2;
+            this.btnIniciar.Text = "TESTAR";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // cameraBox
+            // 
+            this.cameraBox.BackColor = System.Drawing.Color.Transparent;
+            this.cameraBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cameraBox.Location = new System.Drawing.Point(17, 16);
+            this.cameraBox.Margin = new System.Windows.Forms.Padding(40);
+            this.cameraBox.Name = "cameraBox";
+            this.cameraBox.Size = new System.Drawing.Size(325, 300);
+            this.cameraBox.TabIndex = 2;
+            this.cameraBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(362, 411);
+            this.Controls.Add(this.cameraBox);
+            this.Controls.Add(this.btnIniciar);
+            this.ForeColor = System.Drawing.SystemColors.Info;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnIniciar;
+        private Emgu.CV.UI.ImageBox cameraBox;
+    }
+}
+
---
+++ DetectorFacial/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Emgu.CV;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+using System.IO;
+
+namespace DetectorFacial
+{
+    public partial class Form1 : Form
+    {
+        // Variáveis globais.
+
+        Capture camera;
+
+        HaarCascade Modelo;
+
+        Image <Bgr,  Byte> Frame;
+        Image <Gray, byte> result;
+        Image <Gray, byte> RostoDetectado;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // "HaarCascade" é para detecção de faces.
+            Modelo = new HaarCascade("haarcascade_frontalface_default.xml");
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            // Inicia a camera e chama
+            camera = new Capture();
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(processandoCamera);
+        }
+
+        private void processandoCamera(object sender, EventArgs e)
+        {
+            // Análisa os frames e desenha o retângulo, caso encontre um rosto.
+            Frame = camera.QueryFrame().Resize(325, 300, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            RostoDetectado = Frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] FaceAtual = RostoDetectado.DetectHaarCascade(Modelo, 1.1, 5, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+
+            foreach (MCvAvgComp f in FaceAtual[0])
+            {
+                result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.LightGray), 2);             
+            }           
+
+            cameraBox.Image  = Frame;        
+        }
+    }
+}
